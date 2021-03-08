d79d228987b9b33addaaac183812eeda7baa03b6
panjinquan <pan_jinquan@163.com>
2021-03-08 11:37:05 +0800

    ADD SoftMaX函数

--- base_utils/include/math_utils.h
+++ base_utils/include/math_utils.h
@@ -11,11 +11,11 @@
 using namespace std;
 
 /***
- * SoftMaX
+ * SoftMaX函数
  * @param src 输入
  * @param dst 输出
- * @param max_index 最大值的index
- * @param max_value SoftMaX的最大值，max_value = dst[max_index]
+ * @param max_index 输出SoftMaX最大值的index
+ * @param max_value 输出SoftMaX最大值，max_value = dst[max_index]
  */
 void softmax(vector<float> &src, vector<float> &dst, int &max_index, float &max_value);
 
