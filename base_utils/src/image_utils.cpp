865c45fa9c621c41197f9366893c4e4245e5e783
panjinquan <pan_jinquan@163.com>
2021-03-09 15:03:23 +0800

    增加rotate_points

--- base_utils/src/image_utils.cpp
+++ base_utils/src/image_utils.cpp
@@ -3,11 +3,12 @@
 //
 
 #include "image_utils.h"
+#include "math_utils.h"
 
-cv::Mat image_resize(cv::Mat &src, int resize_width, int resize_height) {
+cv::Mat image_resize(cv::Mat &image, int resize_width, int resize_height) {
     cv::Mat dst;
-    auto width = src.cols;
-    auto height = src.rows;
+    auto width = image.cols;
+    auto height = image.rows;
     if (resize_height <= 0 && resize_width <= 0) {
         resize_width = width;
         resize_height = height;
@@ -16,47 +17,88 @@ cv::Mat image_resize(cv::Mat &src, int resize_width, int resize_height) {
     } else if (resize_width <= 0) {
         resize_width = int(width * resize_height / height);
     }
-    cv::resize(src, dst, cv::Size(resize_width, resize_height));
+    cv::resize(image, dst, cv::Size(resize_width, resize_height));
     return dst;
 }
 
-cv::Mat image_crop(cv::Mat &src, cv::Rect rect) {
+
+cv::Mat rotate_image(cv::Mat &image, cv::Point2f center, float angle, cv::Scalar color) {
+    //输出图像的尺寸与原图一样
+    cv::Size dsize(image.cols, image.rows);
+    //指定旋转中心
+    //cv::Point2f center(image.cols / 2., image.rows / 2.);
+    //获取旋转矩阵（2x3矩阵）
+    cv::Mat rot_mat = cv::getRotationMatrix2D(center, angle, 1.0);
+    cv::Mat dst;
+    cv::warpAffine(image, dst, rot_mat, dsize, cv::INTER_LINEAR, cv::BORDER_CONSTANT, color);
+    return dst;
+}
+
+vector<cv::Point2f> rotate_image_points(cv::Mat &image, vector<cv::Point2f> &points, cv::Point2f center, float angle) {
+    image = rotate_image(image, center, angle);
+    return rotate_points(points, center, image.cols, image.rows, angle);;
+}
+
+cv::Point2f rotate_point(cv::Point2f point, cv::Point2f center, int image_width, int image_height, float angle) {
+    // 将图像坐标转换到平面坐标
+    float x1 = point.x;
+    float y1 = image_height - point.y;
+    float x2 = center.x;
+    float y2 = image_height - center.y;
+    float x = (x1 - x2) * cos(PI / 180.0 * angle) - (y1 - y2) * sin(PI / 180.0 * angle) + x2;
+    float y = (x1 - x2) * sin(PI / 180.0 * angle) + (y1 - y2) * cos(PI / 180.0 * angle) + y2;
+    // 将平面坐标转换到图像坐标
+    y = image_height - y;
+    return {x, y};
+}
+
+vector<cv::Point2f> rotate_points(vector<cv::Point2f> &points, cv::Point2f center,
+                                  int image_width, int image_height, float angle) {
+    vector<cv::Point2f> dst_points;
+    for (auto &point:points) {
+        dst_points.push_back(rotate_point(point, center, image_width, image_height, angle));
+    }
+    return dst_points;
+}
+
+
+cv::Mat image_crop(cv::Mat &image, cv::Rect rect) {
     cv::Mat dst;
     //求交集,避免越界
-    rect &= cv::Rect(0, 0, src.cols, src.rows);
-    src(rect).copyTo(dst);
+    rect &= cv::Rect(0, 0, image.cols, image.rows);
+    image(rect).copyTo(dst);
     return dst;
 };
 
 
-cv::Mat image_crop(cv::Mat &src, int x1, int x2, int y1, int y2) {
-    int width = src.cols;
-    int height = src.rows;
+cv::Mat image_crop(cv::Mat &image, int x1, int x2, int y1, int y2) {
+    int width = image.cols;
+    int height = image.rows;
     int left = std::max(0, (int) x1);
     int right = std::min((int) x2, width);
     int top = std::max(0, (int) y1);
     int bottom = std::min(int(y2), height);
     cv::Rect rect(left, top, right - left, bottom - top);
-    cv::Mat dst = image_crop(src, rect);
+    cv::Mat dst = image_crop(image, rect);
     return dst;
 };
 
 
-cv::Mat image_crop_padding(cv::Mat src, cv::Rect rect, cv::Scalar color) {
+cv::Mat image_crop_padding(cv::Mat &image, cv::Rect rect, cv::Scalar color) {
     int borderType = cv::BORDER_CONSTANT;//固定像素填充
     //int borderType = cv::BORDER_REPLICATE;//复制最边缘像素
     //int borderType = cv::BORDER_REFLECT_101;//边缘对称法填充
     int crop_x1 = cv::max(0, rect.x);
     int crop_y1 = cv::max(0, rect.y);
-    int crop_x2 = cv::min(src.cols, rect.x + rect.width); // 图像范围 0到cols-1, 0到rows-1
-    int crop_y2 = cv::min(src.rows, rect.y + rect.height);
+    int crop_x2 = cv::min(image.cols, rect.x + rect.width); // 图像范围 0到cols-1, 0到rows-1
+    int crop_y2 = cv::min(image.rows, rect.y + rect.height);
 
     int left_x = (-rect.x);
     int top_y = (-rect.y);
-    int right_x = rect.x + rect.width - src.cols;
-    int down_y = rect.y + rect.height - src.rows;
+    int right_x = rect.x + rect.width - image.cols;
+    int down_y = rect.y + rect.height - image.rows;
     //cv::Mat roiImage = srcImage(cv::Range(crop_y1, crop_y2 + 1), cv::Range(crop_x1, crop_x2 + 1));
-    cv::Mat roiImage = src(cv::Rect(crop_x1, crop_y1, (crop_x2 - crop_x1), (crop_y2 - crop_y1)));
+    cv::Mat roiImage = image(cv::Rect(crop_x1, crop_y1, (crop_x2 - crop_x1), (crop_y2 - crop_y1)));
     if (top_y > 0 || down_y > 0 || left_x > 0 || right_x > 0)//只要存在边界越界的情况，就需要边界填充
     {
         left_x = (left_x > 0 ? left_x : 0);
@@ -68,19 +110,19 @@ cv::Mat image_crop_padding(cv::Mat src, cv::Rect rect, cv::Scalar color) {
     return roiImage;
 }
 
-cv::Mat image_center_crop(cv::Mat &src, int crop_w, int crop_h) {
-    auto width = src.cols;
-    auto height = src.rows;
-    int x1 = std::max(0, ((width - crop_w + 1) / 2));
-    int y1 = std::max(0, ((height - crop_h + 1) / 2));
-    int x2 = x1 + crop_w;
-    int y2 = y1 + crop_h;
-    cv::Mat dst = image_crop(src, x1, x2, y1, y2);
+cv::Mat image_center_crop(cv::Mat &image, int crop_width, int crop_height) {
+    auto width = image.cols;
+    auto height = image.rows;
+    int x1 = std::max(0, ((width - crop_width + 1) / 2));
+    int y1 = std::max(0, ((height - crop_height + 1) / 2));
+    int x2 = x1 + crop_width;
+    int y2 = y1 + crop_height;
+    cv::Mat dst = image_crop(image, x1, x2, y1, y2);
     return dst;
 }
 
 
-void image_show(string name, cv::Mat image, int waitKey) {
+void image_show(string name, cv::Mat &image, int waitKey) {
 #ifndef PLATFORM_ANDROID
     cv::namedWindow(name, cv::WINDOW_AUTOSIZE);
     cv::Mat img_show = image.clone();
@@ -94,7 +136,7 @@ void image_show(string name, cv::Mat image, int waitKey) {
 #endif
 }
 
-void image_save(string name, cv::Mat image) {
+void image_save(string name, cv::Mat &image) {
 #ifndef PLATFORM_ANDROID
     cv::namedWindow(name, cv::WINDOW_AUTOSIZE);
     cv::imwrite(name, image);
