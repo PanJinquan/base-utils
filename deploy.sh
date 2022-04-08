c9e4dcfba445517107331df26942133f2edeb598
潘金全 <panjinquan@dm-ai.com>
2022-04-08 10:41:16 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 # 制作pip包： https://www.cnblogs.com/sting2me/p/6550897.html
 # 发布pip包： https://packaging.python.org/tutorials/packaging-projects/
 #sudo pip install twine
+pip install dist/pybaseutils-*.*.*.tar.gz
 twine upload dist/*  --verbose
 #Enter your username: PKing
 #Enter your password:
