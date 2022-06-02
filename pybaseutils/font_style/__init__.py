a828c3cfdb9f6267afabcc873a9b82ca12b2822f
潘金全 <panjinquan@dm-ai.com>
2022-06-02 14:05:25 +0800

    update fontstyle

--- pybaseutils/font_style/__init__.py
+++ pybaseutils/font_style/__init__.py
@@ -5,7 +5,11 @@
     @Date   : 2019-05-07 17:40:27
 """
 import os
+
 # windows: C:\Windows\Fonts可查看系统支持的字体
+# Linux  : /usr/share/fonts/truetype可查看系统支持的字体
+
+FONT_ROOT = os.path.dirname(__file__)
 FONT_TYPE = {
     "楷体": "simkai.ttf",
     "宋体": "simsun.ttc",
--- pybaseutils/image_font.py
+++ pybaseutils/image_font.py
@@ -11,16 +11,44 @@ import cv2
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 from pybaseutils import image_utils, file_utils
-from pybaseutils.font_style import FONT_TYPE
-
-FONT_ROOT = os.path.join(os.path.dirname(__file__), "font_style")
+from pybaseutils.font_style import FONT_TYPE, FONT_ROOT
 
 
-def draw_image_text(bgr, point, text, style="楷体", size=20, color=(255, 255, 255)):
+class FontStyle(object):
+    def __init__(self, root=FONT_ROOT, style="宋体", size=20):
+        """
+        :param root: 安装字体的路径
+        :param style: 选择字体风格
+        :param size: 旋转字体大小
+        """
+        self.font_root = root
+        self.font_style = style
+        self.font_size = size
+        self.font_file = os.path.join(self.font_root, FONT_TYPE[style])
+        self.font_type = None
+        if os.path.exists(self.font_file):
+            self.font_type = ImageFont.truetype(self.font_file, size=self.font_size, encoding="utf-8")
+
+    def set_root(self, root):
+        """设置字体的路径"""
+        self.__init__(root=root, style=self.font_style, size=self.font_size)
+
+    def set_font_style(self, style, size):
+        """设置字体风格和大小"""
+        self.__init__(root=self.font_root, style=style, size=size)
+
+    def get_font_style(self):
+        return self.font_type
+
+
+font_style = FontStyle()
+
+
+def draw_image_text(image, point, text, style="楷体", size=20, color=(255, 255, 255)):
     """
     在图像中显示汉字
     https://blog.csdn.net/weixin_44237337/article/details/119817801
-    :param bgr: 字体背景
+    :param image: RGB字体背景
     :param point:
     :param text:
     :param style: 字体风格
@@ -28,14 +56,15 @@ def draw_image_text(bgr, point, text, style="楷体", size=20, color=(255, 255, 
     :param color:
     :return:
     """
-    bgr = Image.fromarray(bgr)
-    draw = ImageDraw.Draw(bgr)
+    image = Image.fromarray(image)
+    draw = ImageDraw.Draw(image)
     # simhei.ttf 是字体，你如果没有字体，需要下载
     # font = ImageFont.truetype("simhei.ttf", size=size, encoding="utf-8")
-    font_type = ImageFont.truetype(os.path.join(FONT_ROOT, FONT_TYPE[style]), size=size, encoding="utf-8")
+    font_style.set_font_style(style=style, size=size)
+    font_type = font_style.get_font_style()
     draw.text(point, text, fill=color, font=font_type)
-    bgr = cv2.cvtColor(np.asarray(bgr), cv2.COLOR_RGB2BGR)
-    return bgr
+    image = np.asarray(image)
+    return image
 
 
 def draw_font(text, style="楷体", scale=1.0, size=20, c1=(255, 255, 255), c2=(0, 0, 0)):
@@ -57,8 +86,8 @@ def draw_font(text, style="楷体", scale=1.0, size=20, c1=(255, 255, 255), c2=(
 
 
 if __name__ == "__main__":
-    size = 1024
+    size = 512
     point = (0, 0)
     string = "飞"
-    image = draw_font(string, style="宋体", size=size, scale=0.8, c2=(0, 255, 0))
+    image = draw_font(string, style="宋体", size=size, scale=0.8, c2=(100, 0, 255))
     image_utils.cv_show_image("image", image, use_rgb=False)
