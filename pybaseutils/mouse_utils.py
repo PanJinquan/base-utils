2d9556bad408a1096bbb362648caa7bb15b78e3f
潘金全 <panjinquan@dm-ai.com>
2022-11-24 11:18:52 +0800

    增加鼠标事件工具

--- pybaseutils/mouse_utils.py
+++ pybaseutils/mouse_utils.py
@@ -14,87 +14,100 @@ from pybaseutils import image_utils
 class DrawImageMouse(object):
     """使用鼠标绘图"""
 
-    def __init__(self, winname, max_point=-1, color=(0, 0, 255), thickness=2):
+    def __init__(self, max_point=-1, line_color=(0, 0, 255) , text_color=(255, 0, 0), thickness=2):
         """
-        :param winname: 窗口名称
         :param max_point: 最多绘图的点数，超过后将绘制无效；默认-1表示无限制
-        :param color: 框的颜色
-        :param thickness: 框的线条粗细
+        :param line_color: 线条的颜色
+        :param text_color: 文本的颜色
+        :param thickness: 线条粗细
         """
-        self.winname = winname
         self.max_point = max_point
-        self.image = None
-        self.polygons = None
-        self.color = color
+        self.line_color = line_color
+        self.text_color = text_color
+        self.focus_color = (0, 255, 0)  # 鼠标焦点的颜色
         self.thickness = thickness
-        self.key = -1
+        self.key = -1  # 键盘值
+        self.orig = None  # 原始图像
+        self.last = None  # 上一帧
+        self.next = None  # 下一帧或当前帧
+        self.polygons = np.zeros(shape=(0, 2), dtype=np.int32)  # 鼠标绘制点集合
 
-    def task(self, image, callback: Callable, destroy=False):
-        self.image = image
-        cv2.namedWindow(self.winname, flags=cv2.WINDOW_NORMAL)
-        cv2.setMouseCallback(self.winname, callback)
+    def clear(self):
+        self.key = -1
+        self.polygons = np.zeros(shape=(0, 2), dtype=np.int32)
+        if self.orig is not None: self.last = self.orig.copy()
+        if self.orig is not None: self.next = self.orig.copy()
+
+    def task(self, image, callback: Callable, winname="winname"):
+        """
+        鼠标监听任务
+        :param image: 图像
+        :param callback: 鼠标回调函数
+        :param winname: 窗口名称
+        :return:
+        """
+        self.orig = image.copy()
+        self.last = image.copy()
+        self.next = image.copy()
+        cv2.namedWindow(winname, flags=cv2.WINDOW_NORMAL)
+        cv2.setMouseCallback(winname, callback, param={"winname": winname})
         while True:
-            key = self.show_image(self.winname, self.image, delay=20)
-            print("key={}".format(key))
-            if key == 13 or key == 32:  # 按空格和回车键退出
+            self.key = self.show_image(winname, self.next, delay=25)
+            print("key={}".format(self.key))
+            if (self.key == 13 or self.key == 32) and len(self.polygons) > 0:  # 按空格32和回车键13退出
                 break
-        if destroy: cv2.destroyAllWindows()
-        cv2.setMouseCallback(self.winname, self.event_default)
+            elif self.key == 27:  # ESC退出
+                exit(0)
+            elif self.key == 99:  # 键盘c重新绘制
+                self.clear()
+        # cv2.destroyAllWindows()
+        cv2.setMouseCallback(winname, self.event_default)
 
     def event_default(self, event, x, y, flags, param):
         pass
 
     def event_draw_rectangle(self, event, x, y, flags, param):
         """绘制矩形框"""
-        if self.polygons is None: self.polygons = np.zeros(shape=(2, 2), dtype=np.int32)  # 多边形轮廓
-        cur_image = self.image.copy()
-        cur_point = (x, y)
+        if len(self.polygons) == 0: self.polygons = np.zeros(shape=(2, 2), dtype=np.int32)  # 多边形轮廓
+        point = (x, y)
         if event == cv2.EVENT_LBUTTONDOWN:  # 左键点击,则在原图打点
             print("1-EVENT_LBUTTONDOWN")
-            self.polygons[0, :] = cur_point
-            cv2.circle(cur_image, cur_point, radius=5, color=(0, 255, 0), thickness=self.thickness)
-            self.key = self.show_image(self.winname, cur_image, delay=5)
+            self.next = self.last.copy()
+            self.polygons[0, :] = point
+            cv2.circle(self.next, point, radius=5, color=self.focus_color, thickness=self.thickness)
         elif event == cv2.EVENT_MOUSEMOVE and (flags & cv2.EVENT_FLAG_LBUTTON):  # 按住左键拖曳，画框
             print("2-EVENT_FLAG_LBUTTON")
-            cv2.circle(cur_image, self.polygons[0, :], radius=4, color=(0, 255, 0), thickness=self.thickness)
-            cv2.circle(cur_image, cur_point, radius=4, color=(0, 255, 0), thickness=self.thickness)
-            cv2.rectangle(cur_image, self.polygons[0, :], cur_point, color=self.color, thickness=self.thickness)
-            self.key = self.show_image(self.winname, cur_image, delay=-1)
+            self.next = self.last.copy()
+            cv2.circle(self.next, self.polygons[0, :], radius=4, color=self.focus_color, thickness=self.thickness)
+            cv2.circle(self.next, point, radius=4, color=self.focus_color, thickness=self.thickness)
+            cv2.rectangle(self.next, self.polygons[0, :], point, color=self.line_color, thickness=self.thickness)
         elif event == cv2.EVENT_LBUTTONUP:  # 左键释放，显示
             print("3-EVENT_LBUTTONUP")
-            self.polygons[1, :] = cur_point
-            cv2.rectangle(cur_image, self.polygons[0, :], cur_point, color=self.color, thickness=self.thickness)
-            self.key = self.show_image(self.winname, cur_image, delay=-1)
-            if np.sum(self.polygons[0, :] - self.polygons[1, :]) != 0:
-                xmin, ymin, xmax, ymax = self.polygons2box(self.polygons)
-                cut = self.image[ymin:ymax, xmin:xmax]
-                self.show_image('ROI', cut, delay=-1)
-        print("point:{},have:{}".format(cur_point, len(self.polygons)))
+            self.next = self.last.copy()
+            self.polygons[1, :] = point
+            cv2.rectangle(self.next, self.polygons[0, :], point, color=self.line_color, thickness=self.thickness)
+        print("point:{},have:{}".format(point, len(self.polygons)))
 
     def event_draw_polygon(self, event, x, y, flags, param):
         """绘制多边形"""
-        if self.polygons is None: self.polygons = np.zeros(shape=(0, 2), dtype=np.int32)  # 多边形轮廓
         exceed = self.max_point > 0 and len(self.polygons) >= self.max_point
-        cur_image = self.image.copy()
-        cur_point = (x, y)
+        self.next = self.last.copy()
+        point = (x, y)
         text = str(len(self.polygons))
         if event == cv2.EVENT_LBUTTONDOWN:  # 左键点击,则在原图打点
             print("1-EVENT_LBUTTONDOWN")
-            cv2.circle(cur_image, cur_point, radius=5, color=(0, 255, 0), thickness=self.thickness)
-            cv2.putText(cur_image, text, cur_point, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+            cv2.circle(self.next, point, radius=5, color=self.focus_color, thickness=self.thickness)
+            cv2.putText(self.next, text, point, cv2.FONT_HERSHEY_SIMPLEX, 0.5, self.text_color, 2)
             if len(self.polygons) > 0:
-                cv2.line(cur_image, self.polygons[-1, :], cur_point, color=(0, 255, 0), thickness=self.thickness)
-            self.key = self.show_image(self.winname, cur_image, delay=10)
-            if not exceed: self.image = cur_image
-        elif event == cv2.EVENT_LBUTTONUP:  # 左键释放
-            print("2-EVENT_FLAG_LBUTTON")
-            if not exceed: self.polygons = np.concatenate([self.polygons, np.array(cur_point).reshape(1, 2)])
+                cv2.line(self.next, self.polygons[-1, :], point, color=self.line_color, thickness=self.thickness)
+            if not exceed:
+                self.last = self.next
+                self.polygons = np.concatenate([self.polygons, np.array(point).reshape(1, 2)])
         else:
-            cv2.circle(cur_image, cur_point, radius=5, color=(0, 255, 0), thickness=self.thickness)
+            cv2.circle(self.next, point, radius=5, color=self.focus_color, thickness=self.thickness)
             if len(self.polygons) > 0:
-                cv2.line(cur_image, self.polygons[-1, :], cur_point, color=(0, 255, 0), thickness=self.thickness)
-            self.key = self.show_image(self.winname, cur_image, delay=-1)
-        print("point:{},have:{}".format(cur_point, len(self.polygons)))
+                cv2.line(self.next, self.polygons[-1, :], point, color=self.line_color, thickness=self.thickness)
+        print("point:{},have:{}".format(point, len(self.polygons)))
 
     def get_polygons(self):
         """获得多边形数据"""
@@ -115,38 +128,41 @@ class DrawImageMouse(object):
         key = cv2.waitKey(delay=delay) if delay >= 0 else -1
         return key
 
-    def draw_image_rectangle_on_mouse(self, image, destroy=False):
+    def draw_image_rectangle_on_mouse(self, image, winname="draw_rectangle"):
         """
         获得鼠标绘制的矩形框box=[xmin,ymin,xmax,ymax]
         :param image:
+        :param winname: 窗口名称
         :return: box is[xmin,ymin,xmax,ymax]
         """
-        self.task(image, callback=self.event_draw_rectangle, destroy=destroy)
+        self.task(image, callback=self.event_draw_rectangle, winname=winname)
         polygons = self.get_polygons()
         box = self.polygons2box(polygons)
         return box
 
-    def draw_image_polygon_on_mouse(self, image, destroy=False):
+    def draw_image_polygon_on_mouse(self, image, winname="draw_polygon"):
         """
         获得鼠标绘制的矩形框box=[xmin,ymin,xmax,ymax]
         :param image:
+        :param winname: 窗口名称
         :return: polygons is (N,2)
         """
-        self.task(image, callback=self.event_draw_polygon, destroy=destroy)
+        self.task(image, callback=self.event_draw_polygon, winname=winname)
         polygons = self.get_polygons()
         return polygons
 
 
-def draw_image_rectangle_on_mouse_example(image_file, winname="image"):
+def draw_image_rectangle_on_mouse_example(image_file, winname="draw_rectangle"):
     """
     获得鼠标绘制的矩形框box=[xmin,ymin,xmax,ymax]
     :param image_file:
+    :param winname: 窗口名称
     :return:
     """
     image = cv2.imread(image_file)
     # 通过鼠标绘制矩形框rect
-    mouse = DrawImageMouse(winname=winname)
-    box = mouse.draw_image_rectangle_on_mouse(image)
+    mouse = DrawImageMouse()
+    box = mouse.draw_image_rectangle_on_mouse(image, winname=winname)
     # 裁剪矩形区域,并绘制最终的矩形框
     roi: np.ndarray = image[box[1]:box[3], box[0]:box[2]]
     if roi.size > 0: mouse.show_image("Image ROI", roi)
@@ -155,16 +171,17 @@ def draw_image_rectangle_on_mouse_example(image_file, winname="image"):
     return box
 
 
-def draw_image_polygon_on_mouse_example(image_file, winname="image"):
+def draw_image_polygon_on_mouse_example(image_file, winname="draw_polygon"):
     """
     获得鼠标绘制的多边形box=[xmin,ymin,xmax,ymax]
     :param image_file:
+    :param winname: 窗口名称
     :return:
     """
     image = cv2.imread(image_file)
     # 通过鼠标绘制多边形
-    mouse = DrawImageMouse(winname=winname, max_point=4)
-    polygons = mouse.draw_image_polygon_on_mouse(image)
+    mouse = DrawImageMouse(max_point=4)
+    polygons = mouse.draw_image_polygon_on_mouse(image, winname=winname)
     image = image_utils.draw_image_points_lines(image, polygons, thickness=2)
     mouse.show_image(winname, image, delay=0)
     return polygons
@@ -172,6 +189,6 @@ def draw_image_polygon_on_mouse_example(image_file, winname="image"):
 
 if __name__ == '__main__':
     image_path = "/media/dm/新加卷/SDK/base-utils/data/test.png"
-    # rect = draw_image_rectangle_on_mouse_example(image_path)
-    rect = draw_image_polygon_on_mouse_example(image_path)
-    # print(rect)
+    out = draw_image_rectangle_on_mouse_example(image_path)
+    # out = draw_image_polygon_on_mouse_example(image_path)
+    # print(out)
--- test_py/demo_document_correct.py
+++ test_py/demo_document_correct.py
@@ -10,7 +10,7 @@ import numpy as np
 from pybaseutils import mouse_utils, file_utils, image_utils
 
 
-def get_target_points(src_pts):
+def get_target_points(src_pts: np.ndarray):
     """
     根据输入的四个角点，计算其矫正后的目标四个角点,src_pts四个点分布：
         0--(w01)---1
@@ -27,10 +27,15 @@ def get_target_points(src_pts):
     h21 = np.sum(np.square(src_pts[2] - src_pts[1]), axis=0)
     w23 = np.sum(np.square(src_pts[2] - src_pts[3]), axis=0)
     xmin, ymin = 0, 0
-    xmax = int(np.sqrt(max(w01, w23)))
-    ymax = int(np.sqrt(max(h03, h21)))
+    if h03 > w01:
+        xmax = np.sqrt(np.mean([w01, w23]))
+        ymax = np.sqrt(np.mean([h03, h21]))
+    else:
+        xmax = np.sqrt(np.mean([w01, w23]))
+        ymax = np.sqrt(np.mean([h03, h21]))
+    # print("h/w:{}".format(abs(23.4 / 17.8 - ymax / xmax)))
     dst_pts = [[xmin, ymin], [xmax, ymin], [xmax, ymax], [xmin, ymax]]
-    dst_pts = np.asarray(dst_pts, dtype=np.int32)
+    dst_pts = np.asarray(dst_pts)
     return dst_pts
 
 
@@ -46,41 +51,48 @@ def document_image_correct(src, src_pts, dst_pts=None, out_size=None, use_ransac
     :param vis: 可视化效果
     :return:
     """
-    if dst_pts is None:
-        dst_pts = get_target_points(src_pts)
+    src_pts = np.float32(src_pts)
+    if dst_pts is None: dst_pts = get_target_points(src_pts)
+    dst_pts = np.float32(dst_pts)
     if out_size is None:
         # xmin = min(pts_dst[:, 0])
         # ymin = min(pts_dst[:, 1])
-        xmax = max(dst_pts[:, 0])
-        ymax = max(dst_pts[:, 1])
+        xmax = int(max(dst_pts[:, 0]))
+        ymax = int(max(dst_pts[:, 1]))
         out_size = [xmax, ymax]
     if use_ransac:
-        m, status = cv2.findHomography(np.float32(src_pts), np.float32(dst_pts), cv2.RANSAC, 5)
+        m, status = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5)
     else:
-        m = cv2.getPerspectiveTransform(np.float32(src_pts), np.float32(dst_pts))
+        m = cv2.getPerspectiveTransform(src_pts, dst_pts)
     dst = cv2.warpPerspective(src, m, dsize=out_size, flags=cv2.INTER_LINEAR)
     if vis:
-        dst = image_utils.draw_points_text(dst, dst_pts, color=(0, 255, 0), thickness=3)
+        dst = image_utils.draw_points_text(dst, np.int32(dst_pts), color=(0, 255, 0), thickness=3)
         image_utils.cv_show_image("correct", dst, use_rgb=False)
     return dst
 
 
-def document_correct_image_example(image, winname="image"):
-    mouse = mouse_utils.DrawImageMouse(winname=winname, max_point=4)
-    pts_src = mouse.draw_image_polygon_on_mouse(image)
-    # pts_src = [[345., 114.], [579., 349.], [346., 582.], [111., 347.]]
-    pts_src = np.asarray(pts_src)
-    print("pts_src={}".format(pts_src))
+def document_correct_image_example(image, winname="document_correct"):
+    pts_src = np.zeros(shape=(0, 2), dtype=np.int32)
+    mouse = mouse_utils.DrawImageMouse(max_point=4, thickness=3)
+    while len(pts_src) < 4:
+        pts_src = mouse.draw_image_polygon_on_mouse(image, winname=winname)
+        # pts_src = [[345., 114.], [579., 349.], [346., 582.], [111., 347.]]
+        pts_src = np.asarray(pts_src)
+        if len(pts_src) < 4:
+            mouse.clear()
+            print("已经标记了文档的{}个角点，需要标记4个角点".format(len(pts_src)))
+            cv2.waitKey(0)
+    print("标记文档的4个角点={}".format(pts_src.tolist()))
     image = image_utils.draw_image_points_lines(image, pts_src, thickness=2)
     image_utils.cv_show_image(winname, image, use_rgb=False, delay=10)
-    # correct_findHomography(img_src, pts_src, pts_dst)
     document_image_correct(image, pts_src, vis=True)
 
 
 if __name__ == '__main__':
-    # image_dir = "/home/dm/nasdata/dataset-dmai/handwriting/word-det/page-correct"
-    image_dir = "/home/dm/nasdata/dataset-dmai/handwriting/word-det/tem"
+    image_dir = "/home/dm/nasdata/dataset-dmai/handwriting/word-det/page-correct1"
+    # image_dir = "/home/dm/nasdata/dataset/csdn/文档矫正"
     image_list = file_utils.get_files_lists(image_dir)
     for image_file in image_list:
         image = cv2.imread(image_file)
         document_correct_image_example(image)
+    cv2.waitKey(0)
