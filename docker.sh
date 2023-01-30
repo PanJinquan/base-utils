69bcfdb3f03258ecdc1cb7879b340c282664730f
潘金全 <panjinquan@dm-ai.com>
2023-01-30 16:11:33 +0800

    update docker

--- docker.sh
+++ docker.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
-#curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
-#sudo apt-key add -
-#distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-#curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
-#sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-#sudo apt-get update
-#sudo apt-get install -y nvidia-container-toolkit
-#sudo systemctl restart docker
 
-#sudo docker build -t="docker.dm-ai.cn/algorithm-research/calligraphy/py36-trt8.4-torch17:v1.2" .
-sudo docker run -it --gpus all -p 8000:80 -v `pwd`:/app docker.dm-ai.cn/algorithm-research/calligraphy/py36-trt8.4-torch17:v1.2 /bin/bash
+image=docker.dm-ai.cn/algorithm-research/calligraphy/py3.8-cuda11.0-cudnn8-trt8.4-torch1.7:v1
+#image=docker.dm-ai.cn/algorithm-research/calligraphy/py3.6-cuda11.2-cudnn8-trt8.4-torch1.7:v1
+sudo docker run -it --gpus all -p 40000:80 -v `pwd`:/app $image /bin/bash
 
+############### 将容器转换为镜像 ###############
+# docker commit -m "info" -a "author" container_id image_id:tag
+# docker push image_id:tag
+
+############### 一些异常处理    ###############
+# Error response from daemon: could not select device driver "" with capabilities: [[gpu]]
+# 解决方法：https://zhuanlan.zhihu.com/p/373493938?utm_id=0
+
+############### docker清除垃圾 ###############
+# apt-get autoclean
+# apt-get clean
+# apt-get autoremove
+# rm -rf ~/.cache
