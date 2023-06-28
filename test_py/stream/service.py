7abd9b470bff61410175d6921b47fd04e7c31214
潘金全 <panjinquan@dm-ai.com>
2023-06-28 17:22:53 +0800

    update service

--- test_py/stream/service.py
+++ test_py/stream/service.py
@@ -1,3 +1,10 @@
+# -*-coding: utf-8 -*-
+"""
+    @Author : PKing
+    @E-mail : 390737991@qq.com
+    @Date   : 2022-12-31 11:37:30
+    @Brief  :
+"""
 import numpy as np
 import cv2, threading, time
 import socket
