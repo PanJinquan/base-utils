5dc5134cb729849a1f7e0f11668ca3b127d00e59
panjinquan <panjinquan@dm-ai.com>
2023-06-29 09:48:40 +0800

    update

--- test_py/demo3.py
+++ test_py/demo3.py
@@ -5,31 +5,28 @@
     @Date   : 2022-12-31 11:37:30
     @Brief  :
 """
-import os
-import cv2
-import time
-from tqdm import tqdm
-from multiprocessing import Pool
-import numpy as np
-from pybaseutils import file_utils, image_utils, base64_utils, time_utils
-from pybaseutils.base_audio import audio_utils
-import socket
-import cv2
-import numpy
-from time import sleep
 
+import numpy as np
+from pybaseutils import image_utils
+import cv2
 
 def get_boxes_up(xyxy, scale=(), cut=0.3):
-    """获得boxes上半部分"""
-    dxyxy = []
+    """
+    获得boxes上半部分
+    :param xyxy: shape is (num-boxes,4),box is (xmin,ymin,xmax,ymax)
+    :param scale: boxes缩放大小
+    :param cut: 裁剪比例
+    :return:
+    """
+    up_boxes = []
     for i in range(len(xyxy)):
         xmin, ymin, xmax, ymax = xyxy[i]
         w, h = (xmax - xmin), (ymax - ymin)
         ymax = max(ymin + h * cut, ymin + w)
-        dxyxy.append([xmin, ymin, xmax, ymax])
-    dxyxy = np.asarray(dxyxy)
-    if scale: dxyxy = image_utils.extend_xyxy(dxyxy, scale=scale)
-    return dxyxy
+        up_boxes.append([xmin, ymin, xmax, ymax])
+    up_boxes = np.asarray(up_boxes)
+    if scale: up_boxes = image_utils.extend_xyxy(up_boxes, scale=scale)
+    return up_boxes
 
 
 if __name__ == '__main__':
