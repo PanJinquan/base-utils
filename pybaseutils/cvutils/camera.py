82b94989fce66e6a343f7c86d56009978f434303
潘金全 <panjinquan@dm-ai.com>
2026-05-19 17:57:30 +0800

    python

--- pybaseutils/cvutils/camera.py
+++ pybaseutils/cvutils/camera.py
@@ -176,8 +176,8 @@ if __name__ == '__main__':
     from pybaseutils.cvutils import video_utils
 
     fps = 10
-    video = video_utils.get_usb_camera()
-    # video = "/home/PKing/Videos/video1.mp4"  # Windows 下可能是 0 或 "video=Integrated Webcam"
+    # video = video_utils.get_usb_camera()
+    video = "/home/PKing/Videos/video1.mp4"  # Windows 下可能是 0 或 "video=Integrated Webcam"
     # video = "../../data/video/kunkun_cut.mp4"  # Windows 下可能是 0 或 "video=Integrated Webcam"
     # cap = CameraCapture(video=video, size=(), scale=1.0, fps=fps)
     cap = CameraCapture(video=video, size=(1920, 1080), scale=1.0, fps=fps)
