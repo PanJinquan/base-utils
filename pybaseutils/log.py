49e634cdd12ffa0ccc34ebfc119ba5f30c6b48f6
潘金全 <panjinquan@dm-ai.com>
2026-05-26 15:28:49 +0800

    python

--- pybaseutils/log.py
+++ pybaseutils/log.py
@@ -21,33 +21,39 @@ LOG_FORMAT = {
 }
 
 
-def set_logger(name=None, level="debug", logfile=None, format="simple", is_main_process=True):
+def set_logger(name=None, level="debug", logfile=None, format="simple", is_main_process=True,
+               rotation="1 day", retention="3 days"):
     """
     logger = set_logger(level="debug", logfile="log.txt")
     url: https://www.cnblogs.com/shiyitongxue/p/17870527.html
     :param level: 设置log输出级别:debug,info,warning,error
     :param logfile: log保存路径，如果为None，则在控制台打印log
     :param is_main_process: 是否是主进程
+    :param rotation: 每rotation，创建新的日志文件
+    :param retention: 日志文件保留时长，默认3天
     :return:
     """
     format = LOG_FORMAT.get(format, LOG_FORMAT.get("line"))
     logger.remove(0)  # 去除默认的LOG
     if is_main_process:
         # 每天创建一个新的文件，一个星期定期清理一次
-        if logfile: logger.add(logfile, level=level.upper(), rotation="1 day", retention="7 days", format=format)
+        if logfile: logger.add(logfile, level=level.upper(), rotation=rotation, retention=retention, format=format)
         logger.add(sys.stderr, level=level.upper(), format=format)
     else:
         logger.add(sys.stderr, level="ERROR", format=format)
     return logger
 
 
-def set_logger_v2(name=None, level="debug", logfile=None, format="simple", is_main_process=True):
+def set_logger_v2(name=None, level="debug", logfile=None, format="simple", is_main_process=True,
+                  rotation="1 day", retention="3 days"):
     """
     logger = set_logger(level="debug", logfile="log.txt")
     url: https://www.cnblogs.com/shiyitongxue/p/17870527.html
     :param level: 设置log输出级别:debug,info,warning,error
     :param logfile: log保存路径，如果为None，则在控制台打印log
     :param is_main_process: 是否是主进程
+    :param rotation: 每rotation，创建新的日志文件
+    :param retention: 日志文件保留时长，默认3天
     :return:
     """
     format = LOG_FORMAT.get(format, LOG_FORMAT.get("simple"))
@@ -57,8 +63,8 @@ def set_logger_v2(name=None, level="debug", logfile=None, format="simple", is_ma
               "format": format, "colorize": True, "level": level.upper()}
         # 输出到文件，文件名app.log
         h2 = {"sink": logfile,
-              "format": format, "colorize": False, "level": level.upper(), "rotation": "1 day",
-              "retention": "7 days"} if logfile else None
+              "format": format, "colorize": False, "level": level.upper(), "rotation": rotation,
+              "retention": retention} if logfile else None
         handlers = [h for h in [h1, h2] if h]
         logger.configure(handlers=handlers)
     else:
